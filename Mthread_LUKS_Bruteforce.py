98d23a369e4af083ee2fcb031078c8bad2d8d64a
Raymond <raa062@student.uib.no>
2015-11-01 15:09:57 +0100

    better handling of threads and regex to remove illegal characters

--- Mthread_LUKS_Bruteforce.py
+++ Mthread_LUKS_Bruteforce.py
@@ -5,24 +5,24 @@ import subprocess
 import sys
 from multiprocessing import Process, Manager
 import time
-from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, FileTransferSpeed, FormatLabel, Percentage, \
-    ProgressBar, ReverseBar, RotatingMarker, \
-    SimpleProgress, Timer
+from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, FileTransferSpeed, FormatLabel, Percentage,ProgressBar, ReverseBar, RotatingMarker,SimpleProgress, Timer
 import os
+import commands
+import re
+
+
 
 def passw(passwd,return_list,headerfile):
 	try:
 		luks_file = headerfile
-		FNULL = open(os.devnull, 'w')
 		#print 'Trying %s...' % repr(passwd)
-		r = subprocess.Popen('echo %s | cryptsetup luksOpen --test-passphrase %s ' % (passwd, luks_file), 
-					shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
-		out, err = r.communicate()
-
+		st = "cryptsetup luksOpen --test-passphrase "+passwd + " "  + luks_file
+		r = subprocess.Popen('echo %s | cryptsetup luksOpen --test-passphrase %s ' % (passwd, luks_file), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	        out, err = r.communicate()
 		if len(err) == 0:
 			return_list[0]="true"
 			return_list[1]=repr(passwd)
-		return
+		return []
 	except:
 		sys.exit(1)
 
@@ -33,44 +33,39 @@ def main(headerfile,uni):
 	headerfile = headerfile
 	words = []
 	
-	chars = set('\\ \'\"><\;|$,\`[](){}_*\t=:& #')
 	jobs=[]
 	luks_file = uni
 	manager = Manager()
 	return_list = manager.list(range(2))
-	
 	for lines in uni:
 		lines = lines.strip()
-		if any((c in chars) for c in lines):
-			continue
-		if len(lines)< 20 and len(lines)> 6 :
+		pattern = r'[^\.\w\!]'
+		sa = re.search('[^\.\w\!]', lines)
+		if not sa and len(lines) > 2:
 			words.append(lines)
 	print "trying ",len(words), "potential passwords"
 	words.reverse()
-	nowtime =time.time()
-	widgets = ['Cracking LUKS-header: ', Percentage(), ' ', Bar(marker=RotatingMarker()), ' ']
+	nowtime = time.time()
+	widgets = ['Cracking LUKS-header: ', Percentage(), ' ', Bar(marker="|"), ' ', ETA(),]
 	pbar = ProgressBar(widgets=widgets, maxval=len(words)).start()
 	for i,passwd in enumerate(words):
 		pbar.update(i+1)
-		passwd = passwd.strip()
-		if any((c in chars) for c in passwd):
-			continue
-		else:
-			t =  Process(target=passw, args = (passwd,return_list,headerfile,))
-			t.daemon = True
-			jobs.append(t)
-			t.start()
-			time.sleep(.1)
-		if len(jobs) >= 15:
-			for j in jobs:
-				j.join()
-				jobs=[]
+		t =  Process(target=passw, args = (passwd,return_list,headerfile,))
+		jobs.append(t)
+		if len(jobs) == 10 or (i == len(words)-1):
+		    for job in jobs:
+                        job.start()
+
+		if (len(jobs) == 10) or (i == len(words)-1):
+		    #print "merge"
+		    for job in jobs:
+		        job.join()
+		        jobs=[]
+
 		if "true" in return_list:
 			i = len(words)
 			pbar.update(i)
 			break
-	if "true" not in return_list:
-		print "No password found"
 	pbar.finish()
 	
 	print  "time used: ", (time.time() - nowtime) / 60,"Min"
@@ -78,8 +73,8 @@ def main(headerfile,uni):
 		print "No password found"
 	else:
 		print "password is ",return_list[1]
-	return 0
-	
+    	return 0
+
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		sys.stderr.write("Need a headerfile \n")
